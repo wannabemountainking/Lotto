c1b8c954d8a350aa6f833bffdcfdea6e6bdedfdc
wannabemountainking <87057243+wannabemountainking@users.noreply.github.com>
2024-11-24 09:19:34 +0900

    computerpick 함수 만드는 중

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const $userChoiceForm = document.querySelector("form#userInput")
-const $userChoice = $userChoiceForm.querySelector("input")
+const $userChoice = $userChoiceForm.querySelector("#userInput > label > input")
 const $computerPick = document.querySelector("div#computerPick div:first-child span")
 const $bonusNumber = document.querySelector("div#computerPick div:nth-child(2) span")
 const $result = document.querySelector("div#result")
 
 let userChoice = [];
 let computerChoice = [];
+let bonusNumber = 0;
 //숫자 뽑기
 let numbers = Array(45)
     .fill(1)
@@ -13,34 +14,58 @@ let numbers = Array(45)
     index + element)
 console.log(numbers)
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// 로또 번호 생성기
+function generateLottoNumbers() {
+  const nums = new Set();
+  while (nums.size < 7) {
+    const pickedNumber = Math.floor(Math.random() * 45) + 1;
+    nums.add(pickedNumber);
 
-const computerPickAmongNumbers = () => {
-  for (let i = 1; i <= 6; i++) {
-    let randomPickedNum = numbers.splice(getRandomInt(0, numbers.length), 1)[0]
-    computerChoice.push(randomPickedNum)
   }
-  return [computerChoice, numbers];
 }
+// function getLottoNumbers() {
+//   const nums = new Set();
+//   while (nums.size < 7) {
+//     bonusNumber = Math.floor(Mathj.random() * 45) + 1;
+//     nums.add(bonusNumber);
+//     if (nums.size === 6) {
+//       const difference = (arr1, arr2) => {
+//         return arr1.filter(x => !arr2.includes(x));
+//       };
+//       nums = difference(nums, [bonusNumber])
+//     }
+//   }
+//   return Array.from(nums);
+// }
 
 function testifyInputNumbers(choice) {
   for (let i = 0; i < choice.length; i++) {
     const arrWithoutIndexI = choice.filter(element => element != choice[i])
     if (isNaN(choice[i])) {
+      alert("숫자가 아닌 요소가 있습니다.")
       return false
     }
     if (choice[i] < 1 || choice[i] > 45) {
+      alert("숫자가 제한 범위를 벗어났습니다. (제한범위: 1 ~ 45)")
       return false
     }
-    if (arrWithoutIndexI.length < 5) {
+    if ((choice.length === 6) && (arrWithoutIndexI.length < 5)) {
+      alert("중복 숫자가 있습니다.")
       return false
     }
   }
-  return (choice.length === 6)
+  if (choice.length === 6) {
+    return true
+  } else {
+    alert("6개의 숫자가 제시되지 못했습니다.")
+    return false
+  }
+}
+
+const showComputerPick = () => {
+  computerChoice = i
+  $computerPick.textContent = computerChoice
+
 }
 
 const onSubmit = (event) => {
@@ -49,6 +74,11 @@ const onSubmit = (event) => {
   if (!testifyInputNumbers(userChoice)) {
     userChoice = []
   }
+  $userChoice.value =""
+  showComputerPick()
+
+  console.log(userChoice)
+  console.log(computerChoice)
 }
 
 $userChoiceForm.addEventListener("submit", onSubmit)
